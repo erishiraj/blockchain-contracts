c4a1604993150bcb49a9ac2a61b3fb31317ccc81
Rishi Raj <erishiraj@gmail.com>
2022-09-21 18:58:48 +0530

    indentation

--- ReceiverPays.sol
+++ ReceiverPays.sol
@@ -55,8 +55,6 @@ contract ReceiverPays {
 
     function prefixed(bytes32 hash) internal pure returns (bytes32) {
         return
-            keccak256(
-                abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
-            );
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
     }
 }
